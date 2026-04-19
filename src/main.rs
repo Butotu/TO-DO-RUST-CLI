300259489260611ba72e396a3b55b50cf258a0ed
Butotu <davilamarcaemail@gmail.com>
2026-04-19 14:53:11 -0300

    todo resto

---
+++ src/main.rs
@@ -0,0 +1,86 @@
+use std::io;
+fn listar_tarefas(lista: &mut Vec<Tarefa>) {
+    let mut lista2 = lista;
+
+    for (i) in lista2 {
+        println!("{}, {} {}", i.titulo, i.descricao, i.feito)
+    }
+}
+fn adicionar_tarefa(lista: &mut Vec<Tarefa>) {
+    let mut tarefa = Tarefa {
+        titulo: String::new(),
+        descricao: String::new(),
+        feito: false,
+    };
+
+    println!("Titulo:");
+    let mut resposta1 = String::new();
+    io::stdin().read_line(&mut resposta1).unwrap();
+
+    println!("Descrição:");
+    let mut resposta2 = String::new();
+    io::stdin().read_line(&mut resposta2).unwrap();
+
+    tarefa.titulo = resposta1.trim().to_string();
+    tarefa.descricao = resposta2.trim().to_string();
+
+    println!(
+        "Adicionado: {} - {} - {}",
+        tarefa.titulo, tarefa.descricao, tarefa.feito
+    );
+
+    lista.push(tarefa);
+}
+struct Tarefa {
+    titulo: String,
+    descricao: String,
+    feito: bool,
+}
+fn adicionar_feito(lista: &mut Vec<Tarefa>) {
+    println!("Qual tarefa quer marcar o feito?");
+
+    for (i, tarefa) in lista.iter().enumerate() {
+        println!("{}: {}, feito: {}", i, tarefa.titulo, tarefa.feito);
+    }
+
+    let mut valor = String::new();
+    io::stdin()
+        .read_line(&mut valor)
+        .expect("Falha ao ler linha");
+
+    let indice_escolhido: usize = match valor.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Por favor, digite um número válido!");
+            return;
+        }
+    };
+
+    if let Some(tarefa) = lista.get_mut(indice_escolhido) {
+        tarefa.feito = true;
+        println!("Tarefa '{}' marcada como concluída!", tarefa.titulo);
+    } else {
+        println!("Índice {} não encontrado na lista.", indice_escolhido);
+    }
+}
+fn main() {
+    let mut listaTarefas: Vec<Tarefa> = Vec::new();
+
+    loop {
+        println!("Qual opção?");
+        println!("adicionar");
+        println!("listar");
+        println!("retirar");
+
+        let mut variavel_input = String::new();
+
+        io::stdin().read_line(&mut variavel_input).unwrap();
+
+        match variavel_input.trim() {
+            "1" => adicionar_tarefa(&mut listaTarefas),
+            "2" => listar_tarefas(&mut listaTarefas),
+            "3" => adicionar_feito(&mut listaTarefas),
+            _ => println!("outro valor"),
+        }
+    }
+}
